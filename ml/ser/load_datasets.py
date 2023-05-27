a682bfa0f14d52fa789f613f8b55a53dc7ac785f
Julia Pavliuk <pavliuk.julia@gmail.com>
2023-05-27 19:27:10 +0300

    Fixed path in dataset loading

--- ml/ser/load_datasets.py
+++ ml/ser/load_datasets.py
@@ -78,7 +78,7 @@ def read_ravdess_data(path_dir: str) -> pd.DataFrame:
 
 def get_dataset(ravdess: bool, crema: bool, savee: bool) -> pd.DataFrame:
     dataset = pd.DataFrame({"Path": pd.Series(dtype="str"), "Emotion": pd.Series(dtype="str")})
-    par_dir = Path(__file__).resolve().parents[1]
+    par_dir = Path(__file__).resolve().parents[2]
     if ravdess:
         path = os.path.join(par_dir, "dataset", "ravdess")
         dataset = pd.concat([dataset, read_ravdess_data(path)], ignore_index=True)
