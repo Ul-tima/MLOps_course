ae86217e5c0922bb804b2e41d9ada574c568de76
Julia Pavliuk <pavliuk.julia@gmail.com>
2023-06-05 13:32:43 +0300

    Update prediction

--- ml/ser/prediction.py
+++ ml/ser/prediction.py
@@ -1,3 +1,4 @@
+import functools
 import pathlib
 
 import librosa
@@ -7,15 +8,26 @@ from keras.saving.saving_api import load_model
 from ml.ser import audio_processing
 from ml.ser.model import load_from_registry
 
+_current_dir = pathlib.Path(__file__).parent
+
+
+@functools.lru_cache
+def get_emotions() -> np.ndarray:
+    path = _current_dir / "data" / "classes.npy"
+    return np.load(path)
+
+
+@functools.lru_cache
+def get_scaling_params() -> tuple[np.ndarray, np.ndarray]:
+    mean = np.load(_current_dir / "data" / "tr_mean.npy")
+    std = np.load(_current_dir / "data" / "tr_std.npy")
+    return mean, std
+
 
 def scale_feature(feature: np.ndarray) -> np.ndarray:
     # Get mean and standard deviation from the training set
-    cur_dir = pathlib.Path(__file__).parent
-    tr_mean = np.load(cur_dir / "data/tr_mean.npy")
-    tr_std = np.load(cur_dir / "data/tr_std.npy")
-
-    feature = (feature - tr_mean) / tr_std
-
+    mean, std = get_scaling_params()
+    feature = (feature - mean) / std
     return feature
 
 
@@ -38,7 +50,8 @@ class Predictor:
         local_path = list(pathlib.Path(local_dir).glob("*.h5"))[0]
         self.model = load_model(local_path)
 
-    def predict(self, file_path, scale=True):
+    def predict(self, file_path, scale=True) -> dict[str, float]:
         features = feture_extraction(file_path, scale)
         y_predict = self.model.predict(features)
-        return y_predict
+        result = dict(zip(get_emotions(), y_predict[0].T))
+        return result
