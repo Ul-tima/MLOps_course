e992b53fc2b254226e78cf5d3529d7a28538c9c0
Julia Pavliuk <pavliuk.julia@gmail.com>
2023-06-05 17:35:37 +0300

    Updated test imports

--- ml/tests/model/test_prediction.py
+++ ml/tests/model/test_prediction.py
@@ -3,41 +3,36 @@ import os
 import numpy as np
 import pytest
 
-from ml.ser.model import load_from_registry
-from ml.ser.prediction import Predictor
-from ml.ser.prediction import feature_extraction
-from ml.ser.prediction import get_emotions
-from ml.ser.prediction import get_scaling_params
-from ml.ser.prediction import scale_feature
+from ml.ser import prediction
 
 
 def test_get_emotions():
-    emotions = get_emotions()
+    emotions = prediction.get_emotions()
     assert isinstance(emotions, np.ndarray)
     assert len(emotions) == 7
 
 
 def test_get_scaling_params():
-    mean, std = get_scaling_params()
+    mean, std = prediction.get_scaling_params()
     assert isinstance(mean, np.ndarray)
     assert isinstance(std, np.ndarray)
     assert mean.shape == std.shape
 
 
 def test_scale_feature(feature):
-    scaled_feature = scale_feature(feature)
+    scaled_feature = prediction.scale_feature(feature)
     assert isinstance(scaled_feature, np.ndarray)
     assert scaled_feature.shape == feature.shape
 
 
 def test_feature_extraction(file_path):
-    features = feature_extraction(file_path, scale=True)
+    features = prediction.feature_extraction(file_path, scale=True)
     assert isinstance(features, np.ndarray)
     assert len(features.shape) == 4
 
 
 def test_predict(file_path):
-    predictor = Predictor("cnn")
+    predictor = prediction.Predictor("cnn")
     result = predictor.predict(file_path, scale=True)
     assert isinstance(result, dict)
     assert len(result) == 7
